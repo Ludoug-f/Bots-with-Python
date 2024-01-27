62880533e8d35c71328832e2043ba5e58eca4e7c
Luciano Freitas <lucianoguns123@gmail.com>
2024-01-26 21:18:52 -0300

    reformulando codigo
    mudando a logica, agora uma função pega os nomes da PokeAPI e usa eles na pesquisa

--- bot_search_bing/Bing_Search_Bot.py
+++ bot_search_bing/Bing_Search_Bot.py
@@ -3,34 +3,49 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import requests
 import random
-import string
 
-# Função para gerar uma string aleatória
-def random_str():
-    return ''.join(random.choice(string.ascii_lowercase) for _ in range(random.randint(3, 10)))
+def Get_Pokenames():
+    try:
+        response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=1000')
+        if response.status_code == 200:
+            pokemons = response.json()['results']
+            return [pokemon['name'] for pokemon in pokemons]
+        else:
+            print('Erro ao obter nomes dos pokemons')
+    except requests.exceptions.RequestException as e:
+        return print(f'Erro ao obter nomes dos pokemons: {e}')
+    
+Pokenames = Get_Pokenames()
+random.shuffle(Pokenames)
+Pokenames = Pokenames[:40]
 
-# Criar uma instância do webdriver Edge
-driver = webdriver.Edge()
+def search():
+    # Criar uma instância do webdriver Edge
+    driver = webdriver.Edge()
+    
+    # Navegar até a página do Bing
+    driver.get('https://bing.com')
 
-# Abrir a página inicial do Bing
-driver.get('https://bing.com')
+    # aguardar usuário logar
+    input('Pressione enter para continuar...')
 
-# Solicita ao usuário que insira um valor e armazena na variável
-input_qnt = input("digite a quantidade de pesquisas que deseja realizar: ")
-search_qnt = int(input_qnt)
+    # Realiza uma pesquisa pra cada nome de pokemon
+    for name in Pokenames:
+        driver.get('https://bing.com')
+        search_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'sb_form_q')))
+        search_box.clear()
+        search_box.send_keys(name)
+        search_box.submit()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'b_results')))
+        time.sleep(2)
 
-# Realizar pesquisas até atingir a quantidade desejada
-for i in range(search_qnt):
-    search_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'sb_form_q')))
-    search_box.clear()
-    search_box.send_keys(random_str())
-    search_box.submit()
-    search_results = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'b_results')))
-    time.sleep(0.5)
+    # Aguardar 2 segundos antes de fechar
+    time.sleep(2)
 
-# Aguardar 1 segundos antes de fechar
-time.sleep(1)
+    # Fechar o navegador
+    driver.quit()
 
-# Fechar o navegador
-driver.quit()
+if __name__ == '__main__':
+    search()
